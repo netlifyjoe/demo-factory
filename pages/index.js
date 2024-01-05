7b0dd628d3ca27629aacaa291d21cfa9defd88f3
Sean Mc Ternan <seanmcternan@gmail.com>
2024-01-05 10:05:17 -0700

    Update Site Title

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Index({ allExperience, profile }) {
     <>
       <Layout>
         <Head>
-          <title>{profile.personal.name}'s Portfolio</title>
+          <title>{profile.personal.name}'s Site</title>
         </Head>
         <Container>
           <Intro profile={profile} />
